080c41f74ded1c7e1157f6a08944a1b2bcd2a38e
alvin <90168547+alvinfebrianto@users.noreply.github.com>
2021-10-24 10:30:03 +0700

    Update AlvinFebrianto_031_Praktikum3 [6].cpp

--- AlvinFebrianto_031_Praktikum3 [6].cpp
+++ AlvinFebrianto_031_Praktikum3 [6].cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <conio.h>
+
 using namespace std ;
  
 int main() {
 	cout << "\n\t\t\t    Tabel Perkalian\n\n" ;
    
-    for (int i=1; i<=10; i++) {
+	for (int i=1; i<=10; i++) {
         for (int j=1; j<=10; j++) {
             cout << " " << i*j << "\t" ;
         }
